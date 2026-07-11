b549d4cd2bc9a875f6bbdb685dc68e217689a6b8
amjadshabozai-coder <amjadshabozai@gmail.com>
2026-07-11 15:27:14 +0500

    Add welcome message and button click alert

--- css/js/script.js
+++ css/js/script.js
@@ -1,1 +1,11 @@
+// Pashtun Blood Network Lahore
 
+console.log("Welcome to Pashtun Blood Network Lahore");
+
+const button = document.querySelector(".btn");
+
+if (button) {
+    button.addEventListener("click", function () {
+        alert("Thank you for supporting Pashtun Blood Network Lahore ❤️");
+    });
+}
